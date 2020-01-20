109c78d392280cde6282576de11db9bfcc506010
rtestard <rtestard@opensense.fr>
2020-01-20 15:43:35 +0100

    I have added checks

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function reverseString(str) {
+    if(!str || str.length < 2 || typeof str !== "string") 
+        return "That's not going to work"
+
     return str.split('')
               .reverse()
               .join('')
 }
 
-/* console.log(reverseString('hello')) */
+console.log(reverseString(12))
--- index.py
+++ index.py
@@ -1,5 +1,8 @@
-def reverse_string(str):
-    listArray = list(str)
+def reverse_string(word):
+    if not str or len(word) < 2 or not isinstance(word, str):
+        return "That's not going to work"
+
+    listArray = list(word)
     listArray.reverse()
     return ''.join(listArray)
 
